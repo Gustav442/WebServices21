a049df07f625093e01409c0d6a880c23600886b8
Gustav Jakobsson <gustavJak94@gmail.com>
2021-05-26 11:20:00 +0200

    ..

---
+++ client/src/main/java/Client.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        try {
+            Socket socket = new Socket("178.174.162.51", 5050);
+
+           var output = new PrintWriter(socket.getOutputStream());
+           output.println("Hello from client!\r\n\r\n");
+           output.flush();
+
+            var inputFromServer = new BufferedReader(new InputStreamReader((socket.getInputStream())));
+
+            while(true) {
+                var line = inputFromServer.readLine();
+                if (line == null || line.isEmpty()) {
+                    break;
+                }
+                System.out.print(line);
+            }
+            inputFromServer.close();
+           output.close();
+           socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- core/src/main/java/Main.java
+++ core/src/main/java/Main.java
@@ -1,10 +1,53 @@
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
 public class Main {
 
     public static void main(String[] args) {
+        //localhost 127.0.0.1
+        //178.174.162.51
 
-        var utils = new Utils();
+        ExecutorService executorService = Executors.newCachedThreadPool();
 
+        try(ServerSocket socket = new ServerSocket(5050)) {
+            while(true) {
+                Socket client = socket.accept();
 
-        System.out.println(utils.message());
+                executorService.submit(()-> handleConnection(client));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void handleConnection(Socket client) {
+        try {
+            System.out.println(client.getInetAddress());
+            System.out.println(Thread.currentThread().getName());
+            var inputFromClient = new BufferedReader(new InputStreamReader((client.getInputStream())));
+
+            while (true) {
+                var line = inputFromClient.readLine();
+                if (line == null || line.isEmpty()) {
+                    break;
+                }
+                System.out.println(line);
+            }
+            var outputToClient = new PrintWriter(client.getOutputStream());
+            outputToClient.print("HTTP/1.1 404 Not Found!\r\n Content-length: 0\r\n\r\n");
+            outputToClient.flush();
+            inputFromClient.close();
+            outputToClient.close();
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
